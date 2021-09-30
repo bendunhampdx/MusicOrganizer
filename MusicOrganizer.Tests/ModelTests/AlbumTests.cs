1e999df5ce8e055e30755cb4d0356a79a2a41290
bendunhampdx <bendunhampdx@gmail.com>
2021-09-30 15:25:41 -0700

    pass tests creating constructor, List and get/set descriptions

--- MusicOrganizer.Tests/ModelTests/AlbumTests.cs
+++ MusicOrganizer.Tests/ModelTests/AlbumTests.cs
@@ -17,7 +17,13 @@ namespace MusicOrganizer.Tests
     [TestMethod]
     public void AlbumConstructor_CreatesInstanceOfAlbum_Album()
     {
+      Album newAlbum = new Album("test");
+      Assert.AreEqual(typeof(Album), newAlbum.GetType());
+    }
 
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
       //Arrange
       string description = "Nevermind";
 
@@ -27,6 +33,33 @@ namespace MusicOrganizer.Tests
 
       //Assert
       Assert.AreEqual(description, result);
+    }
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Nevermind";
+      Album newAlbum = new Album(description);
+
+      //Act
+      string updatedDescription = "In Utero";
+      newAlbum.Description = updatedDescription;
+      string result = newAlbum.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      // Arrange
+      List<Album> newList = new List<Album> { };
+
+      // Act
+      List<Album> result = Album.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
--- MusicOrganizer/Models/Album.cs
+++ MusicOrganizer/Models/Album.cs
@@ -1,17 +1,28 @@
+using System.Collections.Generic;
+
 namespace MusicOrganizer
 {
   public class Album
   {
     public string Description { get; set; }
 
+    private static List<Album> _instances = new List<Album> { };
+
     public Album(string description)
     {
       Description = description;
+      _instances.Add(this);
 
+    }
+
+    public static List<Album> GetAll()
+    {
+      return _instances;
     }
     // public static void ClearAll()
     // {
     //     _instances.Clear();
     // }
+
   }
 }
